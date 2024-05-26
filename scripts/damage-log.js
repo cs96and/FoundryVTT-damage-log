600d3499ea5f1b316019912a1563f333cbbe77ed
Alan Davies <alan.n.davies@gmail.com>
2024-05-26 17:31:05 +0100

    Fixed some warnings in v12 when undoing/re-doing damage.

--- scripts/damage-log.js
+++ scripts/damage-log.js
@@ -619,7 +619,7 @@ class DamageLog {
 
 			// If the user that created the message is connected, let their client update the message.
 			// Otherwise let the GM do it.
-			if (message.user.active ? (message.user.id === game.user.id) : game.user.isGM)
+			if (Util.getMessageAuthor(message).active ? (Util.getMessageAuthor(message).id === game.user.id) : game.user.isGM)
 			{
 				// Changing the message flags will cause the renderChatMessage hook to fire
 				if (flags.revert)
@@ -776,13 +776,13 @@ class DamageLog {
 		const modifier = li.classList.contains("reverted") ? -1 : 1;
 
 		// Check the user that created the message is connected, or there is a GM is connected.
-		if (!message.user.active) {
+		if (!Util.getMessageAuthor(message).active) {
 			const activGMs = game.users.filter(u => u.isGM && u.active);
 			if (!activGMs || (0 === activGMs.length)) {
 				const messageFlags = {
 					undo: ((modifier > 0) ? "undo" : "redo"),
 					damage: li.classList.contains("healing") ? "healing" : "damage",
-					user: message.user.name
+					user: Util.getMessageAuthor(message).name
 				};
 				ui.notifications.error(game.i18n.format("damage-log.error.no-undo-user", messageFlags));
 			}
